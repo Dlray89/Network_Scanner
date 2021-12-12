cfec1332eec8da06bd6d812f850c3a8bd24e17f1
David <dlrayjr89@gmail.com>
2021-12-12 23:45:19 +0000

    Set up the IP field pdst=ip

--- network_scanner.py
+++ network_scanner.py
@@ -9,9 +9,25 @@ import scapy.all as scapy
 def scan_network(ip):
     # set var and create an ARP packet object
     # use scapy to create an ARP object that represent as an ARP Packet
-    # list of all options scapy.ls(scapy.ARP())
-    arp_request = scapy.ARP(pdst=ip)  # this class will print a summary of the object just created
-    print(arp_request.summary())
+    # list of all options from scapy using scapy.ls(scapy.ARP())/ Ether(MAC)= scapy.ls(scapy.Ether())
+    # .summary() will provide a small summary about what's going on.
+
+    arp_request = scapy.ARP(pdst=ip)  # set IP field
+    # print(arp_request.summary())
+
+    # create an eth frame that will send to the broadcast mac address
+    # data and networks is always sent using the mac address
+    # the source mac and des mac is set in the ethernet frame
+
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # combine packets
+    # append using /
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
+
+
+
 
 
 scan_network('10.0.0.1')
