b1088679bc1ffec4ed00239e83371c33aa05a353
David <dlrayjr89@gmail.com>
2021-12-13 00:22:47 +0000

    iterate through list and provides the values being captured

--- network_scanner.py
+++ network_scanner.py
@@ -6,7 +6,7 @@
 import scapy.all as scapy
 
 
-def scan_network(ip):
+def scan_network(ip, answered=None):
     # set var and create an ARP packet object
     # use scapy to create an ARP object that represent as an ARP Packet
     # list of all options from scapy using scapy.ls(scapy.ARP())/ Ether(MAC)= scapy.ls(scapy.Ether())
@@ -23,10 +23,19 @@ def scan_network(ip):
     arp_request_broadcast = broadcast / arp_request
 
     # send arp_request_broadcast packet into the network.
-    # srp() allow us to send packets with a customer Ether layer
+    # srp() allows sending packets with a custom Ether layer
     # set up timeout
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)  # this will return a response from two list
-    print(answered.summary(), unanswered.summary())
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]  # this will return a response from two list
+    # print(answered_list.summary())
+
+    # iterate over the answered_list
+
+    for element in answered_list:
+        # parse the values are being capture in the list
+        # view list of properties using .show()
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("---------------------------------------")
 
 
 scan_network('10.0.0.1/24')
