b751a4c542233faacf2394a051f96ce2e762ebc2
David <dlrayjr89@gmail.com>
2021-12-13 01:10:12 +0000

    create a print function to print and iterate over.

--- network_scanner.py
+++ network_scanner.py
@@ -6,7 +6,7 @@
 import scapy.all as scapy
 
 
-def scan_network(ip, answered=None):
+def scan_network(ip):
     # set var and create an ARP packet object
     # use scapy to create an ARP object that represent as an ARP Packet
     # list of all options from scapy using scapy.ls(scapy.ARP())/ Ether(MAC)= scapy.ls(scapy.Ether())
@@ -29,13 +29,27 @@ def scan_network(ip, answered=None):
     # print(answered_list.summary())
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    # create empty list
+    client_list = []
     # iterate over the answered_list
-    print("IP\t\tMAC ADDRESS\n------------------------------------------")
     for element in answered_list:
         # parse the values are being capture in the list
         # view list of properties using .show()
-        print(element[1].psrc + "\t" + element[1].hwsrc)
-        print("---------------------------------------")
+        # --------------------------------------
+        # set up dictionary and add keys and values
+        client_dict = {"IP Address": element[1].psrc, "MAC Address": element[1].hwsrc}
+        # append dictionary to the main list (client_list)
+        client_list.append(client_dict)
+    return client_list
 
 
-scan_network('10.0.0.1/24')
+def print_results(results_list):
+    # added escape keys for better format
+    print("IP\t\tMAC ADDRESS\n------------------------------------------")
+    # iterate through client list and print
+    for client in results_list:
+        print(client["IP Address"] + "\t" + client["MAC Address"])
+
+
+scan_results = scan_network('10.0.0.1/24')
+print_results(scan_results)
